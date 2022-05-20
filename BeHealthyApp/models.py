237fa314f344e5cdf611ab89552f09b612c75e9a
damprog <damprog.one@gmail.com>
2022-05-20 22:55:36 +0200

    połączenie z bazą do każdego z komponentów

--- BeHealthyApp/models.py
+++ BeHealthyApp/models.py
@@ -11,23 +11,23 @@ class Users(models.Model):
     UserName = models.CharField(max_length=100, null=False)
     Password = models.CharField(max_length=100, null = False)
     Admin = models.BooleanField(null=False)
-    PhotoFileName = models.CharField(max_length=100)
+    PhotoFileName = models.CharField(max_length=400)
     Package = models.ForeignKey(Packages, on_delete=models.CASCADE)
 
 class Recipes(models.Model):
     RecipeId = models.AutoField(primary_key=True)
-    RecipeName = models.CharField(max_length=50, null=False)
-    Category = models.CharField(max_length=50)
+    RecipeName = models.CharField(max_length=100, null=False)
+    Category = models.CharField(max_length=100)
     VotesQuantity = models.FloatField()
     AverageOpinion = models.FloatField()
-    Description = models.CharField(max_length=300)
-    Ingredients = models.CharField(max_length=300)
-    PhotoFileName = models.CharField(max_length=100)
+    Description = models.CharField(max_length=600)
+    Ingredients = models.CharField(max_length=600)
+    PhotoFileName = models.CharField(max_length=400)
     UserId = models.ForeignKey(Users, on_delete=models.CASCADE)
 
 class Feedback(models.Model):
     FeedbackId = models.AutoField(primary_key=True)
     UserId = models.ForeignKey(Users, on_delete=models.CASCADE)
-    Email = models.CharField(max_length=100)
-    Content = models.CharField(max_length=300)
+    Email = models.CharField(max_length=200)
+    Content = models.CharField(max_length=500)
 
